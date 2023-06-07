7f4f62890d6925a4a009d3e0d43439e18684d64f
Igor Teles <igortelesdasilva@outlook.com>
2023-06-07 19:45:48 -0300

    Criando as rotas

--- src/App.js
+++ src/App.js
@@ -1,3 +1,8 @@
+import { Routes, Route, BrowserRouter } from "react-router-dom";
+
+import Home from "./components/pages/Home";
+import Users from "./components/pages/Users";
+import UserBlog from "./components/pages/UserBlog";
 import UserPostForm from "./components/pages/UserPostForm";
 
 import "./styles/normalize.css";
@@ -5,7 +10,16 @@ import "./styles/fontawesome.min.css";
 import "./style/main.css";
 
 function App() {
-  return <UserPostForm />
+  return (<BrowserRouter>
+  <Routes>
+    <Route path="/"elemte= {<Home />} />
+    <Route path="/users"element={<Users />} />
+    <Route path="/users/;userId" element={<UserBlog />} />
+    <Route path="/users/;userId/post" element={<UserPostForm />} />
+
+  </Routes>
+  </BrowserRouter>
+);
 }
 
 export default App;
